dfee8ca6b2936dc2bb00cd2d57ecd6f9b12e7bae
kucw <pk74323jacky@gmail.com>
2019-12-31 17:30:59 +0800

    add twitter oauth1a demo

---
+++ oauth1a-bind-twitter-demo/src/main/java/com/demo/BindTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BindTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BindTwitterApplication.class, args);
+	}
+
+}
---
+++ oauth1a-bind-twitter-demo/src/main/java/com/demo/model/TwitterUser.java
@@ -0,0 +1,54 @@
+package com.demo.model;
+
+import java.io.Serializable;
+
+/**
+ * @author : kucw
+ * @date : 2019/12/31
+ */
+public class TwitterUser implements Serializable {
+    private static final long serialVersionUID = 1L;
+    String screenName;
+    String language;
+    String accessToken;
+    String accessTokenSecret;
+
+    public TwitterUser() {}
+
+    public TwitterUser(String accessToken, String accessTokenSecret) {
+        this.accessToken = accessToken;
+        this.accessTokenSecret = accessTokenSecret;
+    }
+
+    public String getScreenName() {
+        return screenName;
+    }
+
+    public void setScreenName(String screenName) {
+        this.screenName = screenName;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessTokenSecret() {
+        return accessTokenSecret;
+    }
+
+    public void setAccessTokenSecret(String accessTokenSecret) {
+        this.accessTokenSecret = accessTokenSecret;
+    }
+}
